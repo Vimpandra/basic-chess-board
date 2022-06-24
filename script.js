84fbe1723c53262ffe9a5c9a5b5ecb27dda39647
Vimpandra <victorimperatriz@gmail.com>
2022-06-23 21:25:21 -0300

    Started adding functionality to the board

--- script.js
+++ script.js
@@ -70,12 +70,29 @@ let f8 = document.getElementById(`f8`);
 let g8 = document.getElementById(`g8`);
 let h8 = document.getElementById(`h8`);
 
-let square = document.getElementsByClassName(`square`);
+let board = [
+    a1, b1, c1, d1, e1, f1, g1, h1,
+    a2, b2, c2, d2, e2, f2, g2, h2,
+    a3, b3, c3, d3, e3, f3, g3, h3,
+    a4, b4, c4, d4, e4, f4, g4, h4,
+    a5, b5, c5, d5, e5, f5, g5, h5,
+    a6, b6, c6, d6, e6, f6, g6, h6,
+    a7, b7, c7, d7, e7, f7, g7, h7,
+    a8, b8, c8, d8, e8, f8, g8, h8,
+];
 
-a1.addEventListener(`click`, active);
+let activeSquare = document.getElementsByClassName(`active square`);
 
-function active() {
-    if (square.textContent !== ``) {
-        square.className = `active square`;
+board.forEach(item => {
+    item.addEventListener(`click`, movement);
+
+    function movement() {
+        if (activeSquare.length == 0 && item.textContent !== ``) {
+            item.className = `active square`;
+
+        } else if (activeSquare.length > 0) {
+            item.textContent = activeSquare.textContent;
+        }
     }
-}
+
+})
