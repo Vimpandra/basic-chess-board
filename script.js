4343a780edfa3e00b6d4bdf98483cebfade3c988
Vimpandra <victorimperatriz@gmail.com>
2022-06-30 05:46:25 -0300

    It's working thanks to a little help from stack overflow

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-// user clicks the piece they want to move
-// the selected piece gets highlighted
-// start listening for a new click
+const squares = document.getElementsByClassName(`square`);
 
-let squares = document.querySelectorAll(`.square`);
-let activeSquares = document.querySelectorAll(`.active`);
-
-// This condition needs to check if there's any square with the class .active in REAL TIME
-if (activeSquares.length > 0) {
-    squares.forEach(square => {
-        square.addEventListener(`click`, movePiece);
-
-        function movePiece() {
-            let pieceToMove = document.querySelector(`.active`).textContent;
-            square.textContent = pieceToMove;
+Array.from(squares).forEach(sqr => {
+    sqr.addEventListener(`click`, function() {
+        if(this.classList.contains(`active`)) {
+            this.classList.remove(`active`);
+        } else {
+            let currentActive = document.getElementsByClassName(`active`)[0];
+            if (currentActive) {
+                this.textContent = currentActive.textContent;
+                currentActive.textContent = ``;
+                currentActive.classList.remove(`active`);
+            } else {
+                this.classList.add(`active`);
+            }
         }
-    });
-} else {
-    squares.forEach(square => {
-        square.addEventListener(`click`, selectPiece);
-        
-        function selectPiece() {
-            square.className = `active square`;
-        }
-    });
-}
+    })
+});
 
-// the next click indicates the destination to wich the piece moves
-// the piece disappears from the original spot
-// the selected piece overwrittes anything previously on the selected spot
-// program goes back to listening for the piece selection click
